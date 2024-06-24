5fc96738cde9f32a014101888eeacd0553befe96
TujhZreitd <egor.yakushev.96@bk.ru>
2024-06-24 12:44:18 +0300

    2.4. HabrCareerParse [#285213]

--- src/main/java/ru/job4j/grabber/HabrCareerParse.java
+++ src/main/java/ru/job4j/grabber/HabrCareerParse.java
@@ -5,14 +5,25 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
-import java.io.IOException;
+import ru.job4j.grabber.utils.DateTimeParser;
 
-public class HabrCareerParse {
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+public class HabrCareerParse implements Parse {
 
     private static final String SOURCE_LINK = "https://career.habr.com";
     private static final String PREFIX = "/vacancies?page=";
     private static final String SUFFIX = "&q=Java%20developer&type=all";
     private static final int PAGECOUNT = 5;
+
+    private final DateTimeParser dateTimeParser;
+
+    public HabrCareerParse(DateTimeParser dateTimeParser) {
+        this.dateTimeParser = dateTimeParser;
+    }
 
     public static void main(String[] args) throws IOException {
         for (int i = 1; i <= PAGECOUNT; i++) {
@@ -42,5 +53,26 @@ public class HabrCareerParse {
         }
         Element row = document.select(".vacancy-description__text").first();
         return row.text();
+    }
+
+    @Override
+    public List<Post> list(String link) throws IOException {
+        List<Post> result = new ArrayList<>();
+        Connection connection = Jsoup.connect(link);
+        Document document = connection.get();
+        Elements rows = document.select(".vacancy-card__inner");
+        rows.forEach(row -> {
+            Element titleElement = row.select(".vacancy-card__title").first();
+            Element linkElement = titleElement.child(0);
+            Element dateElement = row.select(".vacancy-card__date").first();
+            Element dateTime = dateElement.child(0);
+            String title = titleElement.text();
+            String linkToPost = String.format("%s%s", SOURCE_LINK, linkElement.attr("href"));
+            String description = retrieveDescription(linkToPost);
+            LocalDateTime created = dateTimeParser.parse(dateTime.attr("dateTime"));
+            Post post = new Post(title, linkToPost, description, created);
+            result.add(post);
+        });
+        return result;
     }
 }
