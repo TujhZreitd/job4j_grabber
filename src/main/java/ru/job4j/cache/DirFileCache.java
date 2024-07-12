1c65a3f0d5796e3a22a2b66d46db60a38d931a87
TujhZreitd <egor.yakushev.96@bk.ru>
2024-07-12 11:48:31 +0300

    1. Реализация кеша на SoftReference [#1592]

---
+++ src/main/java/ru/job4j/cache/DirFileCache.java
@@ -0,0 +1,32 @@
+package ru.job4j.cache;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class DirFileCache extends AbstractCache<String, String> {
+
+    private final String cachingDir;
+
+    public DirFileCache(String cachingDir) {
+        this.cachingDir = cachingDir;
+    }
+
+    @Override
+    protected String load(String key) {
+        StringBuilder result = new StringBuilder();
+        StringBuilder path = new StringBuilder();
+        path.append(cachingDir);
+        path.append("/");
+        path.append(key);
+        try (BufferedReader input = new BufferedReader(new FileReader(path.toString()))) {
+            String line;
+            while ((line = input.readLine()) != null) {
+                result.append(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        put(key, result.toString());
+        return key;
+    }
+}
